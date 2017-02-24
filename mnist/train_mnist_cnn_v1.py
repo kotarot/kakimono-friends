8dd80b8dca1cc4d7356375d3ac26d3ed76f1602f
Kotaro Terada <tera@toki.waseda.jp>
2017-02-24 18:59:26 +0900

    Merge noise stuff to one file

--- mnist/train_mnist_cnn_v1.py
+++ mnist/train_mnist_cnn_v1.py
@@ -8,6 +8,8 @@ import chainer.links as L
 from chainer import training
 from chainer.training import extensions
 
+import numpy as np
+
 
 # Network definition
 # http://data.gunosy.io/entry/2016/07/28/180943
@@ -38,11 +40,14 @@ def main():
                         help='Directory to output the result')
     parser.add_argument('--resume', '-r', default='',
                         help='Resume the training from snapshot')
+    parser.add_argument('--noise', '-n', action='store_true', default=False,
+                        help='Use MNIST with noises')
     args = parser.parse_args()
 
     print('GPU: {}'.format(args.gpu))
     print('# Minibatch-size: {}'.format(args.batchsize))
     print('# epoch: {}'.format(args.epoch))
+    print('MNIST noise: {}'.format(args.noise))
     print('')
 
     # Set up a neural network to train
@@ -58,7 +63,41 @@ def main():
     optimizer.setup(model)
 
     # Load the MNIST dataset
-    train, test = chainer.datasets.get_mnist(ndim=3)
+    _train, _test = chainer.datasets.get_mnist(ndim=3)
+
+    # Add noise
+    if not args.noise:
+        train, test = _train, _test
+    else:
+        args.out += '_noise'
+        train_data, train_label = [], []
+        test_data, test_label = [], []
+        np.random.seed(12345)
+        for k in range(4):
+            for train_orig in _train:
+                noised = np.random.normal(0, 0.05, (len(train_orig[0][0]), len(train_orig[0][0][0])))
+                for j, row in enumerate(train_orig[0][0]):
+                    for i, d in enumerate(row):
+                        noised[j][i] += d
+                        if noised[j][i] < 0:
+                            noised[j][i] = 0.0
+                        elif 1 < noised[j][i]:
+                            noised[j][i] = 1.0
+                train_data.append(np.array([noised], dtype=np.float32))
+                train_label.append(np.int32(train_orig[1]))
+            for test_orig in _test:
+                noised = np.random.normal(0, 0.05, (len(test_orig[0][0]), len(test_orig[0][0][0])))
+                for j, row in enumerate(test_orig[0][0]):
+                    for i, d in enumerate(row):
+                        noised[j][i] += d
+                        if noised[j][i] < 0:
+                            noised[j][i] = 0.0
+                        elif 1 < noised[j][i]:
+                            noised[j][i] = 1.0
+                test_data.append(np.array([noised], dtype=np.float32))
+                test_label.append(np.int32(train_orig[1]))
+            train = chainer.datasets.tuple_dataset.TupleDataset(train_data, train_label)
+            test = chainer.datasets.tuple_dataset.TupleDataset(test_data, test_label)
 
     train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
     test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
